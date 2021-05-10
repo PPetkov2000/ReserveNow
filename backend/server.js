81849250f3065dfcc5c0396ac8a57aff3d7d6910
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-05-10 23:23:11 +0300

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const rooms = require("./routes/rooms");
+
+dotenv.config();
+require("./config/database");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/v1/rooms", rooms);
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
