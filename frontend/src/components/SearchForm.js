a00d20e79317832c88be0ab46e0ff30821e9db64
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-05-12 21:51:54 +0300

    Update SearchForm.js

--- frontend/src/components/SearchForm.js
+++ frontend/src/components/SearchForm.js
@@ -1,12 +1,26 @@
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+
 const SearchForm = () => {
+  const [keyword, setKeyword] = useState("");
+  const history = useHistory();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    history.push(`/search/${keyword}`);
+    setKeyword("");
+  };
+
   return (
-    <form className="navbar-search-form">
+    <form className="navbar-search-form" onSubmit={submitHandler}>
       <input
         type="text"
         className="navbar-search-input"
         placeholder="Search..."
+        value={keyword}
+        onChange={(e) => setKeyword(e.target.value)}
       />
-      <button type="submit" className="navbar-search-button">
+      <button type="submit" className="navbar-search-button button-success">
         Search
       </button>
     </form>
