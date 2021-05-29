8d5d2d8989d16276084d63ecf875a9c645cce3ad
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-05-30 02:17:04 +0300

    Update RoomList.js

--- frontend/src/pages/RoomList.js
+++ frontend/src/pages/RoomList.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import RoomListSortings from "../components/RoomListSortings";
 import { getRooms } from "../services/rooms";
 
-const sortingCriterias = ["name", "property_type", "amenity", "price"];
+// const sortingCriterias = ["name", "property_type", "amenity", "price"];
 
 const RoomList = ({ match }) => {
   const keyword = match.params.keyword;
@@ -11,44 +11,55 @@ const RoomList = ({ match }) => {
   const [rooms, setRooms] = useState([]);
   const [amenities, setAmenities] = useState([]);
   const [propertyTypes, setPropertyTypes] = useState([]);
-  const [propertyType, setPropertyType] = useState("");
-  const [amenity, setAmenity] = useState("");
-  const [price, setPrice] = useState("");
 
   useEffect(() => {
-    if (propertyType) {
-      retrieveRooms("property_type", propertyType, page);
-    } else if (amenity) {
-      retrieveRooms("amenity", amenity, page);
-    } else if (price) {
-      retrieveRooms("price", price, page);
-    } else {
-      retrieveRooms("name", keyword, page);
-    }
-  }, [keyword, page, propertyType, amenity, price]);
+    retrieveRoomsByName();
+  }, [keyword]);
 
   const retrieveRooms = async (keywordType, keyword, page) => {
     try {
       const { data } = await getRooms(keywordType, keyword, page);
       setRooms(data.rooms);
-      setPropertyTypes([...new Set(data.rooms.map((x) => x.property_type))]);
-      setAmenities(
-        data.rooms
-          .map((x) => x.amenities)
-          .reduce((acc, curr) => {
-            curr.forEach((x) => {
-              if (x.startsWith("translation missing")) return;
-              if (!acc.includes(x)) {
-                acc.push(x);
-              }
-            });
-            return acc;
-          }, [])
-      );
+      setPropertyTypes(getPropertyTypes(data));
+      setAmenities(getAmenities(data));
       console.log(data);
     } catch (error) {
       console.log(error);
     }
+  };
+
+  const getPropertyTypes = (data) => {
+    return [...new Set(data.rooms.map((room) => room.property_type))];
+  };
+
+  const getAmenities = (data) => {
+    return data.rooms
+      .map((x) => x.amenities)
+      .reduce((acc, curr) => {
+        curr.forEach((x) => {
+          if (x.startsWith("translation missing")) return;
+          if (!acc.includes(x)) {
+            acc.push(x);
+          }
+        });
+        return acc;
+      }, []);
+  };
+
+  const retrieveRoomsByName = () => {
+    return retrieveRooms("name", keyword);
+  };
+
+  const retrieveRoomsByPropertyType = (propertyType) => {
+    return retrieveRooms("property_type", propertyType);
+  };
+
+  const retrieveRoomsByAmenity = (amenity) => {
+    return retrieveRooms("amenity", amenity);
+  };
+
+  const retrieveRoomsByPrice = (price) => {
+    return retrieveRooms("price", price);
   };
 
   return (
@@ -56,9 +67,9 @@ const RoomList = ({ match }) => {
       <RoomListSortings
         propertyTypes={propertyTypes}
         amenities={amenities}
-        setPropertyType={setPropertyType}
-        setAmenity={setAmenity}
-        setPrice={setPrice}
+        retrieveRoomsByPropertyType={retrieveRoomsByPropertyType}
+        retrieveRoomsByAmenity={retrieveRoomsByAmenity}
+        retrieveRoomsByPrice={retrieveRoomsByPrice}
       />
       <div className="room-list-wrapper">
         {rooms.map((room) => (
