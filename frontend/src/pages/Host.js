4223fef6d32fa9f3203c18af91ba2f562ac80e78
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-05-30 02:09:32 +0300

    Update Host.js

--- frontend/src/pages/Host.js
+++ frontend/src/pages/Host.js
@@ -3,34 +3,40 @@ const Host = ({ location }) => {
 
   return (
     <div className="host">
-      <h2 className="host-title">{state.host_name}</h2>
-      <img
-        src={state.host_picture_url}
-        alt={state.host_name}
-        className="host-image"
-      />
-      <p className="host-text">
-        <strong>Total Listings:</strong> {state.host_total_listings_count}
-      </p>
-      <p className="host-text">
-        <strong>Location:</strong> {state.host_location}
-      </p>
-      <p className="host-text">
-        <strong>Neighbourhood:</strong> {state.host_neighbourhood}
-      </p>
-      {state.host_response_rate && (
+      <div className="host-avatar">
+        <img
+          src={state.host_picture_url}
+          alt={state.host_name}
+          className="host-image"
+        />
+        <h2 className="host-title">{state.host_name}</h2>
+      </div>
+      <div className="host-info">
         <p className="host-text">
-          <strong>Response Rate:</strong>: {state.host_response_rate}
+          <strong>Total Listings:</strong> {state.host_total_listings_count}
         </p>
-      )}
-      {state.host_response_time && (
         <p className="host-text">
-          <strong>Response Time:</strong> {state.host_response_time}
+          <strong>Location:</strong> {state.host_location}
         </p>
-      )}
-      <p className="host-text">
-        <strong>About:</strong> {state.host_about}
-      </p>
+        {state.host_neighbourhood && (
+          <p className="host-text">
+            <strong>Neighbourhood:</strong> {state.host_neighbourhood}
+          </p>
+        )}
+        {state.host_response_rate && (
+          <p className="host-text">
+            <strong>Response Rate:</strong>: {state.host_response_rate}
+          </p>
+        )}
+        {state.host_response_time && (
+          <p className="host-text">
+            <strong>Response Time:</strong> {state.host_response_time}
+          </p>
+        )}
+        <p className="host-text">
+          <strong>About:</strong> {state.host_about}
+        </p>
+      </div>
     </div>
   );
 };
