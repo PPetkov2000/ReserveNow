6f5a37d32bd2aac704d9d1585157848140ff57d6
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2022-09-29 02:47:24 +0300

    Update Footer.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,9 +1,9 @@
 const Footer = () => {
   return (
     <footer className="footer">
-      <h3>&copy; Plamen Petkov 2021</h3>
+      <h3>&copy; Plamen Petkov 2021-{new Date().getFullYear().toString()}</h3>
     </footer>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
