5deac942dd43b579bbb6e147ce696847e75ae4d7
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2022-09-29 02:47:06 +0300

    Update SearchForm.js

--- frontend/src/components/SearchForm.js
+++ frontend/src/components/SearchForm.js
@@ -1,31 +1,25 @@
-import { useState } from "react";
-import { useHistory } from "react-router-dom";
+import { useState } from 'react'
+import { useHistory } from 'react-router-dom'
 
 const SearchForm = () => {
-  const [keyword, setKeyword] = useState("");
-  const history = useHistory();
+  const history = useHistory()
+  const [keyword, setKeyword] = useState('')
 
   const submitHandler = (e) => {
-    e.preventDefault();
-    if (!keyword) return;
-    history.push(`/search/${keyword}`);
-    setKeyword("");
-  };
+    e.preventDefault()
+    if (!keyword) return
+    history.push(`/search/${keyword}`)
+    setKeyword('')
+  }
 
   return (
     <form className="navbar-search-form" onSubmit={submitHandler}>
-      <input
-        type="text"
-        className="navbar-search-input"
-        placeholder="Search..."
-        value={keyword}
-        onChange={(e) => setKeyword(e.target.value)}
-      />
+      <input type="text" className="navbar-search-input" placeholder="Search..." value={keyword} onChange={(e) => setKeyword(e.target.value)} />
       <button type="submit" className="navbar-search-button button-success">
         Search
       </button>
     </form>
-  );
-};
+  )
+}
 
-export default SearchForm;
+export default SearchForm
