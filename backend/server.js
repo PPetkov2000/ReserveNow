f992964aa4fa47f118eff61c14cdcd24b292c4be
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2023-12-04 03:45:41 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,19 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const cors = require("cors");
-const rooms = require("./routes/rooms");
+const express = require('express')
+const dotenv = require('dotenv')
+const cors = require('cors')
+const rooms = require('./routes/rooms')
 
-dotenv.config();
-require("./config/database");
+dotenv.config()
+require('./config/database')
 
-const app = express();
+const app = express()
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+// app.use(cors({ origin: '127.0.0.1', credentials: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true, limit: '50mb' }))
 
-app.use("/api/v1/rooms", rooms);
+app.use('/api/v1/rooms', rooms)
 
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 4000
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
