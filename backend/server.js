4630c1cf722acfd3470813fada444081cc15dbc3
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2024-01-31 20:50:04 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,8 +10,8 @@ const app = express()
 
 app.use(cors())
 // app.use(cors({ origin: '127.0.0.1', credentials: true }));
-app.use(express.json())
-app.use(express.urlencoded({ extended: true, limit: '50mb' }))
+app.use(express.json({ limit: '10kb' }))
+app.use(express.urlencoded({ extended: true, limit: '10kb' }))
 
 app.use('/api/v1/rooms', rooms)
 
