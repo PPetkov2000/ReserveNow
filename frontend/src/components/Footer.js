6b13c020b631aba2d3d6cd06ea462fdeb3d7f7d7
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2025-06-14 23:30:23 +0300

    Update Footer.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="footer">
-      <h3>&copy; Plamen Petkov 2021-{new Date().getFullYear().toString()}</h3>
+      <h3>&copy; TV Tracker 2021-{new Date().getFullYear().toString()}</h3>
     </footer>
   )
 }
